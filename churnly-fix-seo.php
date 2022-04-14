0e1c464dbcfe6290ff6369f56f5a0441bb98c07e
Joey Gilham <jpg@neilpie.co.uk>
2022-04-14 11:41:55 +0100

    Update churnly-fix-seo.php

--- churnly-fix-seo.php
+++ churnly-fix-seo.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Churnly Fix Notifications
+Plugin Name: Churnly: YoastSEO Compatibility Fix
 Description: Adds a fix to bypass Yoast SEO's reload so that notifications display as expected on Update Credit Card page
 Version: 0.1
 Author: The team at PIE
